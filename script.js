a33a218d2a6c8965fe49f9dd476c6f4b5ed2d5e0
Abhi <abhi3s6y9@gmail.com>
2023-06-27 12:05:43 +0530

    Create search bar and display cards for meals from backend as well

--- script.js
+++ script.js
@@ -3,3 +3,78 @@ function showSidebar() {
     document.getElementById("sidebar").classList.toggle("show-sidebar");
 }
 
+// Checks if ID is in the Favourite list
+function isFav(list, id) {
+    let res = false;
+    for (let i = 0; i < list.length; i++) {
+        if (id == list[i]) {
+            res = true;
+        }
+    }
+    return res;
+}
+
+// It returns truncated string greater than 50
+function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+}
+
+// Configure local storage for favourite items
+const dbObjectFavList = "favouritesList";
+if (localStorage.getItem(dbObjectFavList) == null) {
+    localStorage.setItem(dbObjectFavList, JSON.stringify([]));
+}
+
+// Function to fetch data/meals from api
+const fetchMealsFromApi = async (url, value) => {
+    const response = await fetch(`${url + value}`);
+    const meals = await response.json();
+    return meals;
+}
+
+// Function to show Meals based on search
+async function showMealList() {
+    const list = JSON.parse(localStorage.getItem(dbObjectFavList));
+    const inputValue = document.getElementById("search-input").value;
+    const url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+    const mealsData = await fetchMealsFromApi(url, inputValue);
+    let html = '';
+    if (mealsData.meals) {
+        html = mealsData.meals.map(element => {
+
+            return `
+
+            <div class="card">
+            <div class="card-top"  onclick="showMealDetails(${element.idMeal}, '${inputValue}')">
+                <div class="dish-photo" >
+                    <img src="${element.strMealThumb}" alt="">
+                </div>
+                <div class="dish-name">
+                    ${element.strMeal}
+                </div>
+                <div class="dish-details">
+                    ${truncate(element.strInstructions, 50)}
+                    
+                    <span class="button" onclick="showMealDetails(${element.idMeal}, '${inputValue}')">Know More</span>
+
+                </div>
+            </div>
+            <div class="card-bottom">
+                <div class="like">
+
+                <i class="fa-solid fa-heart ${isFav(list, element.idMeal) ? 'active' : ''} " onclick="addRemoveToFavList(${element.idMeal})"></i>
+                
+                </div>
+                <div class="play">
+                    <a href="${element.strYoutube}">
+                        <i class="fa-brands fa-youtube"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+            `
+        }).join('');
+
+        document.getElementById('cards-holder').innerHTML = html;
+    }
+}
